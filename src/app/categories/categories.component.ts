78c949f82f204a03c9f9e6f2fe8f90425f46c2c0
mrbasit <itechnician030@gmail.com>
2023-01-21 19:59:15 +0500

    Search issue resolved,filtration changes,data chng

--- src/app/categories/categories.component.ts
+++ src/app/categories/categories.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { HomeComponent } from '../home/home.component';
 import '../modals/categories';
-import { FilterqueryService } from '../services/filterquery.service';
+import { AllCardContent ,FilterqueryService } from '../services/filterquery.service';
 @Component({
   selector: 'categories',
   templateUrl: './categories.component.html',
@@ -8,15 +9,22 @@ import { FilterqueryService } from '../services/filterquery.service';
 })
 export class CategoriesComponent implements OnInit{
 
-  cardContent: Categories[];
-  filteredCategories?: Categories[];
+  cardContent: Card[] = AllCardContent;
+  filteredCategories?: Card[];
+  aa = "any";
 
   constructor(private queryService: FilterqueryService ){
-    this.cardContent = CardContent;
+    this.cardContent = AllCardContent;
   }
 
   ngOnInit(): void {
     this.filter();
+      this.filteredCategories =  this.cardContent.filter(val => { 
+        console.log(val)
+        return val.type?.toLowerCase() == this.queryService.queryText.toLowerCase();
+      }
+    );
+    
   }
 
   filter(){
@@ -24,119 +32,8 @@ export class CategoriesComponent implements OnInit{
 
     this.filteredCategories =  this.cardContent.filter(val=> { 
       console.log(val)
-       return val.subCategory?.toLowerCase() == query.toLowerCase();
+       return val.type?.toLowerCase() == query.toLowerCase();
     });
     })
   }
 }
-
-const CardContent: Categories[] = [
-  {
-    subCategory: 'icons',
-    category: 'assets',
-    card: [
-      {
-      image: 'https://material.angular.io/assets/img/examples/shiba2.jpg',
-      type: 'icons',
-      path: 'assets/icon',
-      discription: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit Asperiores quod Atque, nobis ducimus error quos commodialias veniam cupiditate consequuntur'
-      },
-      {
-        image: 'https://material.angular.io/assets/img/examples/shiba2.jpg',
-        type: 'icons',
-        path: 'assets/icon',
-        discription: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit Asperiores quod Atque, nobis ducimus error quos commodialias veniam cupiditate consequuntur'
-        },
-        {
-          image: 'https://material.angular.io/assets/img/examples/shiba2.jpg',
-          type: 'icons',
-          path: 'assets/icon',
-          discription: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit Asperiores quod Atque, nobis ducimus error quos commodialias veniam cupiditate consequuntur'
-          },
-          {
-            image: 'https://material.angular.io/assets/img/examples/shiba2.jpg',
-            type: 'icons',
-            path: 'assets/icon',
-            discription: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit Asperiores quod Atque, nobis ducimus error quos commodialias veniam cupiditate consequuntur'
-            }
-  ]
-},
-{
-  subCategory: 'Assets',
-  category: 'assets',
-  card: [
-    {
-    image: 'https://material.angular.io/assets/img/examples/shiba2.jpg',
-    type: 'icons',
-    path: 'assets/icon',
-    discription: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit Asperiores quod Atque, nobis ducimus error quos commodialias veniam cupiditate consequuntur'
-    },
-    {
-      image: 'https://material.angular.io/assets/img/examples/shiba2.jpg',
-      type: 'icons',
-      path: 'assets/icon',
-      discription: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit Asperiores quod Atque, nobis ducimus error quos commodialias veniam cupiditate consequuntur'
-      },
-    {
-    image: 'https://material.angular.io/assets/img/examples/shiba2.jpg',
-    type: 'icons',
-    path: 'assets/icon',
-    discription: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit Asperiores quod Atque, nobis ducimus error quos commodialias veniam cupiditate consequuntur'
-    },
-    {
-      image: 'https://material.angular.io/assets/img/examples/shiba2.jpg',
-      type: 'icons',
-      path: 'assets/icon',
-      discription: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit Asperiores quod Atque, nobis ducimus error quos commodialias veniam cupiditate consequuntur'
-      },
-    {
-      image: 'https://material.angular.io/assets/img/examples/shiba2.jpg',
-      type: 'icons',
-      path: 'assets/icon',
-      discription: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit Asperiores quod Atque, nobis ducimus error quos commodialias veniam cupiditate consequuntur'
-      }
-]
-},
-{
-  subCategory: 'Music',
-  category: 'assets',
-  card: [
-    {
-    image: 'https://material.angular.io/assets/img/examples/shiba2.jpg',
-    type: 'icons',
-    path: 'assets/icon',
-    discription: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit Asperiores quod Atque, nobis ducimus error quos commodialias veniam cupiditate consequuntur'
-    },
-    {
-    image: 'https://material.angular.io/assets/img/examples/shiba2.jpg',
-    type: 'icons',
-    path: 'assets/icon',
-    discription: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit Asperiores quod Atque, nobis ducimus error quos commodialias veniam cupiditate consequuntur'
-    },
-    {
-    image: 'https://material.angular.io/assets/img/examples/shiba2.jpg',
-    type: 'icons',
-    path: 'assets/icon',
-    discription: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit Asperiores quod Atque, nobis ducimus error quos commodialias veniam cupiditate consequuntur'
-    },
-    {
-    image: 'https://material.angular.io/assets/img/examples/shiba2.jpg',
-    type: 'icons',
-    path: 'assets/icon',
-    discription: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit Asperiores quod Atque, nobis ducimus error quos commodialias veniam cupiditate consequuntur'
-    },
-    {
-    image: 'https://material.angular.io/assets/img/examples/shiba2.jpg',
-    type: 'icons',
-    path: 'assets/icon',
-    discription: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit Asperiores quod Atque, nobis ducimus error quos commodialias veniam cupiditate consequuntur'
-    },
-    {
-    image: 'https://material.angular.io/assets/img/examples/shiba2.jpg',
-    type: 'icons',
-    path: 'assets/icon',
-    discription: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit Asperiores quod Atque, nobis ducimus error quos commodialias veniam cupiditate consequuntur'
-    },
-]
-}
-]
--- src/app/services/filterquery.service.ts
+++ src/app/services/filterquery.service.ts
@@ -7,6 +7,7 @@ import { Subject, BehaviorSubject } from 'rxjs';
 export class FilterqueryService {
 
   query$ = new Subject();
+  queryText:string="";
   categoriesDataSource$ = new Subject<DevResources[]>();
   apiDataSource$ = new Subject<DevResources[]>();
 
@@ -15,6 +16,8 @@ export class FilterqueryService {
 
   public setQuery(query: string){
     this.query$.next(query)
+    this.queryText=query;
+
   }
 
   public setCategories(categories: DevResources[]){
@@ -24,4 +27,78 @@ export class FilterqueryService {
     this.apiDataSource$.next(APIs)
   }
 }
+
+export let AllCardContent:Card[]=[
+  {
+    image: 'https://www.shutterstock.com/image-vector/programming-code-coding-hacker-background-260nw-1714491562.jpg',
+    type: 'icons',
+    path: 'assets/icon',
+    discription: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit Asperiores quod Atque, nobis ducimus error quos commodialias veniam cupiditate consequuntur',
+    date:"2024-07-21"
+  },
+  {
+    image: 'https://www.shutterstock.com/image-vector/programming-code-coding-hacker-background-260nw-1714491562.jpg',
+    type: 'fonts',
+    path: 'assets/fonts',
+    discription: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit Asperiores quod Atque, nobis ducimus error quos commodialias veniam cupiditate consequuntur',
+    date:"2024-06-22"
+  },
+  {
+    image: 'https://www.shutterstock.com/image-vector/programming-code-coding-hacker-background-260nw-1714491562.jpg',
+    type: 'icons',
+    path: 'assets/icon',
+    discription: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit Asperiores quod Atque, nobis ducimus error quos commodialias veniam cupiditate consequuntur',
+    date:"2024-07-21"
+  },
+  {
+    image: 'https://www.shutterstock.com/image-vector/programming-code-coding-hacker-background-260nw-1714491562.jpg',
+    type: 'fonts',
+    path: 'assets/fonts',
+    discription: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit Asperiores quod Atque, nobis ducimus error quos commodialias veniam cupiditate consequuntur',
+    date:"2024-06-21"
+  },
+  {
+    image: 'https://www.shutterstock.com/image-vector/programming-code-coding-hacker-background-260nw-1714491562.jpg',
+    type: 'icons',
+    path: 'assets/icon',
+    discription: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit Asperiores quod Atque, nobis ducimus error quos commodialias veniam cupiditate consequuntur',
+    date:"2024-07-21"
+  },
+  {
+    image: 'https://www.shutterstock.com/image-vector/programming-code-coding-hacker-background-260nw-1714491562.jpg',
+    type: 'fonts',
+    path: 'assets/fonts',
+    discription: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit Asperiores quod Atque, nobis ducimus error quos commodialias veniam cupiditate consequuntur',
+    date:"2024-06-21"
+  },
+  {
+    image: 'https://www.shutterstock.com/image-vector/programming-code-coding-hacker-background-260nw-1714491562.jpg',
+    type: 'icons',
+    path: 'assets/icon',
+    discription: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit Asperiores quod Atque, nobis ducimus error quos commodialias veniam cupiditate consequuntur',
+    date:"2024-07-21"
+  },
+  {
+    image: 'https://www.shutterstock.com/image-vector/programming-code-coding-hacker-background-260nw-1714491562.jpg',
+    type: 'fonts',
+    path: 'assets/fonts',
+    discription: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit Asperiores quod Atque, nobis ducimus error quos commodialias veniam cupiditate consequuntur',
+    date:"2024-06-21"
+  },
+
+  {
+    image: 'https://www.shutterstock.com/image-vector/programming-code-coding-hacker-background-260nw-1714491562.jpg',
+    type: 'audio',
+    path: 'assets/icon',
+    discription: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit Asperiores quod Atque, nobis ducimus error quos commodialias veniam cupiditate consequuntur',
+    date:"2024-10-21"
+  },
+  {
+    image: 'https://www.shutterstock.com/image-vector/programming-code-coding-hacker-background-260nw-1714491562.jpg',
+    type: 'audio',
+    path: 'assets/fonts',
+    discription: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit Asperiores quod Atque, nobis ducimus error quos commodialias veniam cupiditate consequuntur',
+    date:"2024-06-21"
+  }
+]
 
