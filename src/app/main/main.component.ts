b60425ac85ff81a641947a213c722ae10e6d5409
Munawar Hassan <munawarubit956@gmail.com>
2023-01-23 15:22:55 +0500

    addfunctionalcorssbuttoninmobileresponsive

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,26 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild, AfterViewInit } from '@angular/core';
 import { FilterqueryService } from '../services/filterquery.service';
 @Component({
   selector: 'main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.scss']
 })
-export class MainComponent implements OnInit {
+export class MainComponent implements OnInit, AfterViewInit {
   query!: string;
   isOpen: boolean;
+
+  @ViewChild("sidenav",{static: false, read: ElementRef}) sideNav;
   constructor(private service: FilterqueryService){
     this.service.query$.subscribe(x => {
       this.query = x as string;
       console.log(x)
     })
   }
+ 
   ngOnInit(): void {
 
     this.service.isOpenSiveNav$.subscribe((val: boolean) => {
       this.isOpen = val;
       console.log(this.isOpen)
      })
-    
+  }
+
+  ngAfterViewInit(): void {
+    console.log(this.sideNav)
+   }
+
+  close(){
+    this.sideNav.nativeElement.classList.remove('display-side-nav');
+    this.service.isOpenSiveNav$.next(false);
+
   }
 
 }
